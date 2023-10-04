0cd0b5f5643b376eb56f936c7fb2a7582cb3ce14
dickchest <dickchest@gmail.com>
2023-10-04 16:22:42 +0200

    add liquibase Interview tables

---
+++ src/main/resources/db/changelog/v-1.0/70-create-interview-table.sql
@@ -0,0 +1,28 @@
+create table interview (
+    id integer not null auto_increment,
+    comments varchar(255),
+    date date,
+    location varchar(255),
+    status enum ('ACCEPTED','CANCELED','COMPLETED','NO_SHOW','PENDING_REVIEW','REJECTED','RESCHEDULED','SCHEDULED') not null,
+    time time(6),
+    candidate_id integer,
+    employee_id integer,
+    primary key (id)
+) engine=InnoDB
+;
+
+alter table interview
+add constraint UK_pe0kf6sjqo34dsqis5gsraeoe unique (employee_id)
+;
+
+alter table interview
+add constraint FKjod0wwyxvbi7qyx9cmlnt8xq4
+foreign key (candidate_id)
+references candidate (id)
+;
+
+alter table interview
+add constraint FKefxxgrsb5h84h5rj06h8ittv9
+foreign key (employee_id)
+references employee (id)
+;
