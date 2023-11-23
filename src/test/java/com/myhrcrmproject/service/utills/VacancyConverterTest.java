3cc27e7f8813414f14e0fca9cac40d8d22423e94
dickchest <dickchest@gmail.com>
2023-11-23 22:01:31 +0100

    add: VacancyConverterTest

---
+++ src/test/java/com/myhrcrmproject/service/utills/VacancyConverterTest.java
@@ -0,0 +1,29 @@
+package com.myhrcrmproject.service.utills;
+
+import com.myhrcrmproject.domain.Vacancy;
+import com.myhrcrmproject.dto.vacancyDTO.VacancyResponseDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class VacancyConverterTest {
+    @InjectMocks
+    private VacancyConverter vacancyConverter;
+
+    @Test
+    void testToDto_employeeIsNull() {
+        Vacancy entity = new Vacancy();
+        entity.setId(1);
+        entity.setJobTitle("Test title");
+
+        VacancyResponseDTO responseDTO = vacancyConverter.toDTO(entity);
+
+        assertNotNull(responseDTO);
+    }
+}
